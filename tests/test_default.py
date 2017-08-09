229bb47cb14ed6b15edab4ee13cd3b6182bb0159
Jakub Krol <jkrol@sointeractive.pl>
2017-08-09 09:35:41 +0200

    add tests

--- tests/test_default.py
+++ tests/test_default.py
@@ -5,3 +5,25 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 def test_linux(SystemInfo):
     assert SystemInfo.type == 'linux'
+
+
+def test_packages(Package):
+    present = [
+        "hdparm",
+        "sysbench",
+        "wget"
+    ]
+    if present:
+        for package in present:
+            p = Package(package)
+            assert p.is_installed
+
+
+def test_files(File):
+    absent = [
+        "/tmp/server_information"
+    ]
+    if absent:
+        for file in absent:
+            d = File(file)
+            assert not d.exists
